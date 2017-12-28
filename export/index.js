454a9bf2ae32761d26551dad63830744fe68b128
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-12-28 23:52:44 +0900

    add .nojekyll

--- export/index.js
+++ export/index.js
@@ -6,15 +6,24 @@ import path from "path";
 
 const copyFile = (src: string, dst: string): Promise<string> =>
     new Promise((resolve, reject) => {
-        fs.copyFile(src, dst, err => {
+        (fs: any).copyFile(src, dst, err => {
             if (err) return reject(err);
             resolve(dst);
+        });
+    });
+
+const writeFile = (path, txt): Promise<string> =>
+    new Promise((resolve, reject) => {
+        fs.writeFile(path, txt, err => {
+            if (err) return reject(err);
+            resolve(path);
         });
     });
 
 (async () => {
     const { posts, statics } = await list();
     const result = await Promise.all([
+        writeFile("out/.nojekyll", ""),
         ...posts.map(p => post_json(p)),
         ...statics.map(p => copyFile(p, path.join("out", p))),
     ]);
